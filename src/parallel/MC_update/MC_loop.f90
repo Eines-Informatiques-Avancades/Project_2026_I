6ff4d2d44dd0c382eeb8bcdb3c7a1c9f499a08b9
Adrià <git@verbto.be>
2026-04-11 12:48:29 +0200

    donete gut

--- src/parallel/MC_update/MC_loop.f90
+++ src/parallel/MC_update/MC_loop.f90
@@ -45,9 +45,10 @@ module mcloop
 
     contains
 
-    subroutine runMC(rank_world, nproc_world, ntry, naccept, REPLICA_COMM) 
+    subroutine runMC(rank_world, nproc_world, num_replicas, ntry, naccept, REPLICA_COMM) 
         ! Runs the basic Monte Carlo loops and stages (equilibration, production and sampling)
-        integer, intent(in) :: rank_world, nproc_world, REPLICA_COMM
+        integer, intent(in) :: rank_world, nproc_world, num_replicas, REPLICA_COMM
+        integer :: ierr, rank_rep
         integer :: i, j, k, n_mcs
         integer, intent(inout) :: ntry, naccept
         double precision :: R_old(3, N), phi_old, deltaPhi, dE
@@ -72,7 +73,7 @@ module mcloop
             end do
             ! Sample observables during equilibration & production to track changes
             if (mod(i, NSAVE) == 0) then
-                call sample(i, rank_world, nproc_world, REPLICA_COMM)
+                call sample(i, rank_world, nproc_world)
             end if
             if ((i.ge.N_MCEQUI).and.(mod(i, NSAVE) == 0)) then
                 ! Only store config. for the master of the replica
@@ -80,8 +81,10 @@ module mcloop
             end if
 
             ! Try replica exchange
-            if (mod(i, N_SWAP) == 0) then
-                call replica_exchange(rank_world, nproc_world, i, REPLICA_COMM)
+            if (N_SWAP > 0) then
+                if (mod(i, N_SWAP) == 0) then
+                    call replica_exchange(rank_world, nproc_world, num_replicas, i)
+                end if
             end if
         end do
     end subroutine
@@ -119,7 +122,7 @@ module mcloop
         R_old = R
         ! Check we need to recompute the Verlet list before the rotation
         if (isVlist.eq.1) then 
-            call checkUpdateVlist(k, R_old(:,k+3))
+            call checkUpdateVlist()
         end if
 
         ! Calculate local loop bounds for I = 1 to k+1
@@ -140,7 +143,7 @@ module mcloop
         do I = i_start, i_end
             if (isVlist.eq.1) then
                 ! before was k-1 as input instead of I
-                call enerPartVlist(R(1, I), R(2, I), R(3, I), I, enI)
+                call enerPartVlist(R(1, I), R(2, I), R(3, I), I, k, enI)
             else if (isVlist.eq.0) then
                 call enerPart(R(1, I), R(2, I), R(3, I), I, max(k, I+1), enI)
             end if
@@ -157,7 +160,7 @@ module mcloop
 
         ! Check we need to recompute the Verlet list after the rotation
         if (isVlist.eq.1) then 
-            call checkUpdateVlist(k, R(:,k+3))
+            call checkUpdateVlist()
         end if
 
         ! compute *new* torsion and non-bonded energies
@@ -165,7 +168,7 @@ module mcloop
         local_enb_new = 0.d0
         do I = i_start, i_end
             if (isVlist.eq.1) then
-                call enerPartVlist(R(1, I), R(2, I), R(3, I), I, enI)
+                call enerPartVlist(R(1, I), R(2, I), R(3, I), I, k, enI)
             else if (isVlist.eq.0) then
                 call enerPart(R(1, I), R(2, I), R(3, I), I, max(k, I+1), enI)
             end if
@@ -227,16 +230,16 @@ module mcloop
         end if
     end subroutine accept_reject
 
-    subroutine sample(step, rank, nproc)
+    subroutine sample(step, rank, nproc_world)
         ! Extracts data during the "production" phase for statistical analysis
         implicit none
-        integer, intent(in) :: step, rank, nproc
+        integer, intent(in) :: step, rank, nproc_world
         integer :: i, ierr
         double precision :: Ree, Rg, Rcm(3)
         character(len=512) :: path_ener, path_tors, path_struct
         
         ! Dynamic buffers for gathered values
-        double precision :: all_En(nproc), all_Ree(nproc), all_Rg(nproc)
+        double precision :: all_En(nproc_world), all_Ree(nproc_world), all_Rg(nproc_world)
 
         !! End-to-end Distance and Radius of Gyration
         ! End-to-end Distance (Ree)
@@ -284,10 +287,10 @@ module mcloop
         end if
     end subroutine sample
 
-    subroutine replica_exchange(rank, nproc, step)
+    subroutine replica_exchange(rank_world, nproc_world, num_replicas, step)
         use nonBonded, only: new_vlist
         implicit none
-        integer, intent(in) :: rank, nproc, step
+        integer, intent(in) :: rank_world, nproc_world, num_replicas, step
         integer :: partner, ierr
         integer :: status(MPI_STATUS_SIZE)
         double precision :: E_me, E_partner
@@ -300,22 +303,22 @@ module mcloop
         ! Determine partner safely based on step counter
         if (mod(step/N_SWAP, 2) == 0) then
             ! Even pairs: 0-1, 2-3, 4-5
-            if (mod(rank, 2) == 0) then
-                partner = rank + 1
+            if (mod(rank_world, 2) == 0) then
+                partner = rank_world + 1
             else
-                partner = rank - 1
+                partner = rank_world - 1
             end if
         else
             ! Odd pairs: 1-2, 3-4, 5-6
-            if (mod(rank, 2) == 1) then
-                partner = rank + 1
+            if (mod(rank_world, 2) == 1) then
+                partner = rank_world + 1
             else
-                partner = rank - 1
+                partner = rank_world - 1
             end if
         end if
         
         ! Ensure boundary partners don't go out of bounds
-        if (partner >= 0 .and. partner < nproc) then
+        if (partner >= 0 .and. partner < nproc_world) then
             ! We have a valid swap pair
             E_me = En
             T_me = TEMP
@@ -330,8 +333,13 @@ module mcloop
                               MPI_COMM_WORLD, status, ierr)
                               
             ! Lower rank evaluates the Metropolis criterion
-            if (rank == min(rank, partner)) then
-                swap_prob = exp( (1.d0/T_me - 1.d0/T_partner) * (E_me - E_partner) )
+            if (rank_world == min(rank_world, partner)) then
+                ! If num_replicas =1 it reduces to see which replica has the lowest energy (Metropolis-like)
+                if (num_replicas > 1) then
+                    swap_prob = exp( (1.d0/T_me - 1.d0/T_partner) * (E_me - E_partner) )
+                else if (num_replicas == 1) then
+                    swap_prob = exp((E_me - E_partner) / T_me)
+                end if
                 
                 if (swap_prob >= 1.d0) then
                     accept_swap = 1
@@ -346,7 +354,7 @@ module mcloop
             end if
             
             ! Broadcast decision to the higher rank
-            if (rank == min(rank, partner)) then
+            if (rank_world == min(rank_world, partner)) then
                 call MPI_Send(accept_swap, 1, MPI_INTEGER, partner, 2, MPI_COMM_WORLD, ierr)
                 swap_decision = accept_swap
             else
--- src/parallel/mainglobal.f90
+++ src/parallel/mainglobal.f90
@@ -1,7 +1,4 @@
 program mainglobal
-    ! cooligula: this is a work in progress of the ultimate "global" main program, 
-    ! for now it just allows the sequential execution of the
-    ! initialisation and the energy modules
 
     use mpi
     ! Initialization modules (@J-dot-Barrientos)
@@ -29,6 +26,14 @@ program mainglobal
     call MPI_Comm_rank(MPI_COMM_WORLD, rank_world, ierr)
     call MPI_Comm_size(MPI_COMM_WORLD, nproc_world, ierr)
 
+    ! Leer parámetros solo en rank 0
+    if (rank_world == 0) then
+        call readInput()
+    end if
+
+    ! Enviar parámetros a todos los procesos
+    call broadcastInput()
+
     !--Hierarchical MPI setup
     num_replicas = nproc_world / nproc_per_replica
     ! 'color' identifies the replica group (0, 1, 2...)
@@ -46,21 +51,13 @@ program mainglobal
     call random_seed(size=nseed)
     allocate(seed(nseed))
     do i = 1, nseed
-        seed(i) = 12345 + rank*1000 + i
+        seed(i) = 12345 + rank_world*1000 + i
     end do
     call random_seed(put=seed)
     ! Different RNG per replica
 
     ! IO (each replica writes in its own folder)
     call init_io(color)
-
-    ! Leer parámetros solo en rank 0
-    if (rank == 0) then
-        call readInput()
-    end if
-
-    ! Enviar parámetros a todos los procesos
-    call broadcastInput()
 
     ! Verificación
     if (rank_world == 0) then
@@ -98,18 +95,18 @@ program mainglobal
     call shiftLenJon()
     ! write(91, *) 'RC, ECUT:', RC, ECUT
     call totEnergy(En, Eb, Enb)     
-    ! write(91, *), "Initial Enb, Eb, En:", Enb, Eb, En
+    write(91, *), "Initial Enb, Eb, En:", Enb, Eb, En
     ! Energies initialization
 
     ! MC evolution
-    call runMC(rank, nproc, ntry, naccept)
+    call runMC(rank_world, nproc_world, num_replicas, ntry, naccept, REPLICA_COMM)
     ! write(91, *), "Final: ntry, naccept:", ntry, naccept
     ! MC evolution
 
     ! Test final energies and acceptance ratio
     call totEnergy(En, Eb, Enb)
-    !write(91, *) "Final Enb, Eb, En:", Enb, Eb, En
-    !write(91, *) "Attempts, accepted, ratio(%):", ntry, naccept, 100.d0*naccept/ntry
+    ! write(91, *) "Final Enb, Eb, En:", Enb, Eb, En
+    ! write(91, *) "Attempts, accepted, ratio(%):", ntry, naccept, 100.d0*naccept/ntry
     ! Test final energies and acceptance ratio
 
     ! Final CPU counter
@@ -117,6 +114,7 @@ program mainglobal
 
     if (rank_rep == 0) then
         write(91, *) "Rank:", color
+        write(91, *) 'RC, ECUT:', RC, ECUT
         write(91, *) "Attempts, accepted, ratio(%):", ntry, naccept, 100.d0*naccept/ntry
         write(91, *) "Total CPU time:", t2-t1
 
